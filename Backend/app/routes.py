a06b98048e3fc2591306ab4433e45e0aa14b5f4e
CallumGreenwald <callum.greenwald@gmail.com>
2024-04-16 18:35:42 +0800

    added other HTML pages to flask

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -4,4 +4,24 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template("index.html",title="Callum Test",user=user)
+    return render_template("index.html",title="Study Group maker",user=user)
+
+@app.route('/making_request')
+def making_request():
+    return render_template("making_request.html",title="making request")
+
+@app.route('/password_reset')
+def password_reset():
+    return render_template("password_reset.html",title="reset password")
+
+@app.route('/responding_request')
+def responding_request():
+    return render_template("responding_request.html",title="respond to request")
+
+@app.route('/user_creation')
+def user_creation():
+    return render_template("user_creation.html",title="account")
+
+@app.route('/user_login')
+def user_login():
+    return render_template("user_login.html",title="login")
