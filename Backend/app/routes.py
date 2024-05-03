2f9e39682360ee2e4d213b1cfcab8ac9c0d5dd06
CallumGreenwald <callum.greenwald@gmail.com>
2024-05-03 11:03:28 +0800

    Making user page
    I have made the initial HTML for the user page. This page will show the email of the current user as well as what groups they are waiting for and a list of notification informing them about groups that have been made.

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import current_user, login_user
 from app import app
 @app.route('/')
 @app.route('/index')
@@ -28,3 +29,21 @@ def user_creation():
 @app.route('/user_login')
 def user_login():
     return render_template("user_login.html",title="login")
+
+@app.route('/user/<username>')
+#@login_required
+def user_page(username):
+    user = "TEMPORARY"
+    groups = ["group name 1",
+              "group name 2",
+              "group name 3"
+             ]
+
+    notifications = [{"Title":"Example Title1", "date":"29th" "time":"0800 - 0900","emails":"email1 <br> email2"},
+                     {"Title":"Example Title2", "date":"30th" "time":"0930 - 1100","emails":"email3 <br> email4"},
+                     {"Title":"Example Title3", "date":"31th" "time":"2200 - 2345","emails":"email5 <br> email6"},
+                     {"Title":"Example Title4", "date":"1st" "time":"0000 - 0100","emails":"email7 <br> email8"},
+                     ]
+    
+    return render_template("user_page.html",title = user , user=user , groups=groups , notifications = notifications)
+
