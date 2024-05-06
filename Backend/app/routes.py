2bb351d2bbce3d46ebf8dfb6895f4c4ed4931f0d
CallumGreenwald <callum.greenwald@gmail.com>
2024-05-06 15:32:12 +0800

    started userpage CSS

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask_login import current_user, login_user
 from app import app
-@app.route('/')
+#@app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Debug User'}
@@ -30,19 +30,20 @@ def user_creation():
 def user_login():
     return render_template("user_login.html",title="login")
 
+@app.route('/')
 @app.route('/user/<username>')
 #@login_required
-def user_page(username):
+def user_page(username = "TEST"):
     user = "TEMPORARY"
     groups = ["group name 1",
               "group name 2",
               "group name 3"
              ]
 
-    notifications = [{"Title":"Example Title1", "date":"29th", "time":"0800 - 0900","emails":"email1 <br> email2"},
-                     {"Title":"Example Title2", "date":"30th", "time":"0930 - 1100","emails":"email3 <br> email4"},
-                     {"Title":"Example Title3", "date":"31th", "time":"2200 - 2345","emails":"email5 <br> email6"},
-                     {"Title":"Example Title4", "date":" 1st", "time":"0000 - 0100","emails":"email7 <br> email8"},
+    notifications = [{"Title":"Example Title1", "date":"29th", "time":"0800 - 0900","emails":["email","email"]},
+                     {"Title":"Example Title2", "date":"30th", "time":"0930 - 1100","emails":["email","email"]},
+                     {"Title":"Example Title3", "date":"31th", "time":"2200 - 2345","emails":["email","email"]},
+                     {"Title":"Example Title4", "date":" 1st", "time":"0000 - 0100","emails":["email","email"]},
                      ]
     
     return render_template("user_page.html",title = user , user=user , groups=groups , notifications = notifications)
