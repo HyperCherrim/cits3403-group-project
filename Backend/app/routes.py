db131a43c50d6b4b25c647fb39f041fddc7b291e
Krish Dubey <122972264+Krish-Dubey@users.noreply.github.com>
2024-05-18 12:59:36 +0800

    Added about page, put it in navbar, created route
    About.html is for users to see what our website is about, and describes our libraries, references and instructions on how to use our website.

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,65 +1,33 @@
 from flask import render_template, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user, login_required
-from app import app, db, login
+from flask_login import current_user, login_user, logout_user
+from app import app, db
 import sqlalchemy as alchemy
-
-from app.models import Users, Groups, ReplyMessages, TimeSlot
-from app.forms import userLogin, userRegister, submitTimes, TimeSlotForm, WeekForm
-from datetime import time
-
+from app.models import Users, Groups
+from app.forms import userLogin, userRegister
 @app.route('/')
 @app.route('/index')
 def index():
     availableGroups = Groups.query.all()
     user = Users.query.all()
-    units = db.session.query(Groups.tagOne).order_by(alchemy.desc(Groups.tagOne)).all()
-    return render_template("index.html",title="Study Group Organiser Application",user=user,groups=availableGroups,cssFile="../static/main.css",jsFile="../static/main.js", units=units)
+    #user = {'username': 'Debug User'} # Since the user query is being weird, going back to the debug dictionary
+    #availableGroups = [{"unit":"CITS2200: Data Structures and Algorithms"}, 
+                       #{"unit":"CITS1003: Introduction to Cybersecurity"}, 
+                       #{"unit":"MATH1721: Mathematics Foundations: Methods"}] # Populate this later once options for new tags are added
+    return render_template("index.html",title="Study Group Organiser Application",user=user,groups=availableGroups,cssFile="../static/index.css",jsFile="../static/main.js")
 
-@app.route('/createGroup', methods=['GET', 'POST'])
+@app.route('/about')
+def about():
+    return render_template("about.html",title="About Us")
+
+@app.route('/createGroup')
 def createGroup():
-    form = WeekForm()
-    print("hello 1")
-    print(form.validate_on_submit())
-    if form.validate_on_submit():
-        print("hello 1")
-        # Create and save the Group instance
-        group = Groups(
-            userID='user_id_example',  # This should be dynamically set, e.g., from the logged-in user
-            groupTitle=form.groupTitle.data,
-            tagOne=form.groupTag1.data,
-            tagTwo=form.groupTag2.data,
-            tagThree=form.groupTag3.data,
-            description=form.description.data
-        )
-        db.session.add(group)
-        db.session.commit()
+    return render_template("createGroup.html",title="Create a Group - Study Group Organiser",cssFile="../static/createGroup.css",jsFile="../static/populateTable.js")
 
-        # Save the TimeSlot instances
-        for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
-            for slot in getattr(form, day).entries:
-                start_time = time.fromisoformat(slot.start_time.data)
-                end_time = time.fromisoformat(slot.end_time.data)
-                print(start_time)
-                print(start_time >= end_time)
-                if not start_time == end_time:
-                    if start_time > end_time:
-                        flash(f'End time must be after start time for {day.capitalize()}.', 'danger')
-                        return render_template('createGroup.html', form=form)
-                    new_slot = TimeSlot(
-                        groupID=group.groupID,
-                        day=day,
-                        start_time=start_time,
-                        end_time=end_time
-                    )
-                db.session.add(new_slot)
-        db.session.commit()
-        flash('Time slots and group saved!', 'success')
-        return redirect(url_for('index'))
-    return render_template("createGroup.html",title="Create a Group - Study Group Organiser",cssFile="../static/main.css",jsFile="../static/populateTable.js", form=form)
-
+@app.route('/password_reset')
+def password_reset():
+    return render_template("password_reset.html",title="Reset Password",cssFile="../static/password_reset.css",jsFile="../static/main.js")
 
 @app.route('/responding_request')
-@login_required
 def responding_request():
     return render_template("responding_request.html",title="Reply to Group Request",cssFile="../static/responding_request.css",jsFile="../static/main.js")
 
@@ -84,50 +52,13 @@ def user_login():
     form = userLogin()
     if form.validate_on_submit() == True:
         user = db.session.scalar(alchemy.select(Users).where(Users.userName == form.studentUser.data))
-        if user is None or not user.getPassword(form.studentPwd.data):
+        if user == None or not user.getPassword(form.studentPwd.data):
             flash("Invalid login details")
-            return redirect(url_for('user_login'))
         login_user(user)
         return redirect(url_for('index'))
     return render_template("user_login.html",title="Log In - Study Group Organiser",form=form,cssFile="../static/login.css",jsFile="../static/main.js")
 
 @app.route('/logout')
-@login_required
 def userLogout():
     logout_user()
     return(redirect(url_for('index')))
-
-@app.route('/user')
-#@login_required
-def user_page():
-    #username = "TEST"
-    username = current_user.userName
-    groups = [
-    "The Quantum Thinkers",
-    "Code Crusaders",
-    "Logic Lords",
-    "The Algorithmic Alliance",
-    "Data Dynamos",
-    "Coding Conquerors"
-    ]
-    notifications = [{"Title":"CITS:2200 exam", "timedate":["31st at 0100-0800","19th at 1100-2100"],"emails":["23631345@student.uwa.edu.au","12345678@email.com.au"]},
-                     {"Title":"Book club", "timedate":["31st at 0100-0800","19th at 1100-2100"],"emails":["23631345@student.uwa.edu.au","12345678@email.com.au"]},
-                     {"Title":"team all the marks", "timedate":["31st at 0100-0800","19th at 1100-2100"],"emails":["23631345@student.uwa.edu.au","12345678@email.com.au"]},
-                     {"Title":"Example Title4", "timedate":["31st at 0100-0800","19th at 1100-2100"],"emails":["23631345@student.uwa.edu.au","12345678@email.com.au"]},
-                     ]
-    # groups = []
-    # notifications = []
-
-
-    return render_template("user_page.html",title = username, user=username, groups=groups, cssFile="../static/userpage.css" ,notifications=notifications)
-
-
-
-@login.user_loader
-def getID(userID):
-    return db.session.get(Users, int(userID))
-
-@app.errorhandler(401)
-def unauthorisedError(error):
-    flash("You are not signed in!")
-    return redirect(url_for("user_login"))
