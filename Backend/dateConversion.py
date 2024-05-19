5c68226734d4c481176470d492b111e64e6034ab
Krish Dubey <122972264+Krish-Dubey@users.noreply.github.com>
2024-05-19 12:16:39 +0800

    Removed print statements
    They are no longer required.

--- Backend/dateConversion.py
+++ Backend/dateConversion.py
@@ -6,13 +6,11 @@ def encodeTimes(rawTimes):
         #newTime = None # Null for now since I'm not sure how the datetime is being passed
         newTime = str(item)
         encodedTimes.append(newTime)
-        print(type(encodedTimes[0]))
     return encodedTimes
 
 def decodeTimes(dtTimes):
     decodedTimes = []
     for item in dtTimes:
-        print(item)
         decodedTime = ""
         decodedTime = datetime.strptime(item, '%d/%m/%y')
         decodedTimes.append(decodedTime)
